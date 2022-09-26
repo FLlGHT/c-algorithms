55f26d993adb8f2b6281f45e9f826798577361a7
fedorenko <fedorenko@reshenie-soft.ru>
2022-09-26 16:31:39 +0300

    #323. Number of Connected Components in an Undirected Graph

--- unionfind/SatisfiabilityOfEqualityEquations.h
+++ unionfind/SatisfiabilityOfEqualityEquations.h
@@ -9,6 +9,34 @@
 #include <wsman.h>
 using namespace std;
 
+/**
+ *
+ * 990. Satisfiability of Equality Equations
+ *
+ * <p>
+ * You are given an array of strings equations that represent relationships between variables
+ * where each string equations[i] is of length 4 and takes one of two different forms: "xi==yi" or "xi!=yi".
+ * Here, xi and yi are lowercase letters (not necessarily different) that represent one-letter variable names.
+ * <p>
+ * Return true if it is possible to assign integers to variable names to satisfy all the given equations, or false otherwise.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: equations = ["a==b","b!=a"]
+ * Output: false
+ * Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.
+ * There is no way to assign the variables to satisfy both equations.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: equations = ["b==a","a==b"]
+ * Output: true
+ * Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
+ * <p>
+ *
+ */
+
+
 class SatisfiabilityOfEqualityEquations {
 public:
     bool equationsPossible(vector<string>& equations) {
